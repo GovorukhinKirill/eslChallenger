3f0643176fc7d698c08465472c4253ffd398d535
Kirill <kirillgovorukhin007@gmail.com>
2025-10-29 18:23:52 +0100

    update

--- eslchallenger/src/components/matchManager.jsx
+++ eslchallenger/src/components/matchManager.jsx
@@ -5,25 +5,54 @@ import "../App.css"
 
 
 function MatchManager() {
+    let winnersArray = []
     console.log(teams) 
     const [match , setMatch] = useState([]);
     const [teamsArray, setTeamsArray] = useState(teams);
     const [round, setRound] = useState(0);
+    let countresult = 0
     function simulateRandomMatch(TeamA , TeamB) {
         return Math.random() > 0.5 ?TeamA : TeamB
     }
     function startMatch() {
-        for( let i = 0 ; i < teams.length; i += 2 ) {
+        let countteam = 0
+        countresult = countresult + 1
+        if(countteam ){
+            for( let i = 0 ; i < teams.length; i += 2 ) {
+             countteam = countteam + 1
             const team1 = teams[i];
             const team2 = teams[i + 1];
             console.log(teams[i].name + " VS " + teams[i + 1].name )
             if (teams[i].strenght > teams[i + 1].strenght) {
                 console.log(teams[i].name + " winner " )
-                //document.querySelector(teams[i].name).className += " green ";
+
+                if (document.querySelector(`#${teams[i].name}`)){
+                    console.log(document.querySelector(`.${teams[i].name}`))
+                    document.querySelector(`.${teams[i].name}`).classList.add("green")
+                    winnersArray.push(teams[i])
+                    console.log(winnersArray)
+                }
+
+                if (document.querySelector(`#${teams[i + 1].name}`)){
+                    console.log(document.querySelector(`.${teams[i + 1].name}`))
+                    document.querySelector(`.${teams[i + 1].name}`).classList.add("red")
+                }
+                 
             }
             else if (teams[i].strenght < teams[i + 1].strenght) {
                 console.log(teams[i + 1].name + "winner")
                 //document.querySelector(teams[i + 1].name).classList = " green ";
+                 if (document.querySelector(`#${teams[i].name}`)){
+                    console.log(document.querySelector(`.${teams[i].name}`))
+                    document.querySelector(`.${teams[i].name}`).classList.add("red")
+                }
+
+                if (document.querySelector(`#${teams[i + 1].name}`)){
+                    console.log(document.querySelector(`.${teams[i + 1].name}`))
+                    document.querySelector(`.${teams[i + 1].name}`).classList.add("green")
+                    winnersArray.push(teams[i+1])
+                    console.log(winnersArray)
+                }
             }
             else if (teams[i].strenght == teams[i + 1].strenght)
             {
@@ -31,10 +60,90 @@ function MatchManager() {
             }
             else {
                 console.log('Error')
+
             }
+            console.log(countteam)
+
+            if(countteam<5){
+                document.querySelector(".tournamenttable .left").innerHTML += `
+                <div>${teams[i].name}</div>
+                <div>${teams[i + 1].name}</div>
+            `
+            }
+            else{
+                document.querySelector(".tournamenttable .right" ).innerHTML += `
+                <div>${teams[i].name}</div>
+                <div>${teams[i + 1].name}</div>
+            `
+            }
+        
         }
+        }
+        // else if (countresult == 2){
+        //     for( let i = 0 ; i < teams.length; i += 2 ) {
+        //      countteam = countteam + 1
+        //     const team1 = teams[i];
+        //     const team2 = teams[i + 1];
+        //     console.log(teams[i].name + " VS " + teams[i + 1].name )
+        //     if (teams[i].strenght > teams[i + 1].strenght) {
+        //         console.log(teams[i].name + " winner " )
+
+        //         if (document.querySelector(`#${teams[i].name}`)){
+        //             console.log(document.querySelector(`.${teams[i].name}`))
+        //             document.querySelector(`.${teams[i].name}`).classList.add("green")
+        //             winnersArray.push(teams[i+1])
+        //         }
+
+        //         if (document.querySelector(`#${teams[i + 1].name}`)){
+        //             console.log(document.querySelector(`.${teams[i + 1].name}`))
+        //             document.querySelector(`.${teams[i + 1].name}`).classList.add("red")
+        //         }
+                 
+        //     }
+        //     else if (teams[i].strenght < teams[i + 1].strenght) {
+        //         console.log(teams[i + 1].name + "winner")
+        //         //document.querySelector(teams[i + 1].name).classList = " green ";
+        //          if (document.querySelector(`#${teams[i].name}`)){
+        //             console.log(document.querySelector(`.${teams[i].name}`))
+        //             document.querySelector(`.${teams[i].name}`).classList.add("red")
+        //         }
+
+        //         if (document.querySelector(`#${teams[i + 1].name}`)){
+        //             console.log(document.querySelector(`.${teams[i + 1].name}`))
+        //             document.querySelector(`.${teams[i + 1].name}`).classList.add("green")
+        //             winnersArray.push(teams[i+1])
+        //         }
+
+        //     }
+        //     else if (teams[i].strenght == teams[i + 1].strenght)
+        //     {
+        //         console.log("начинается 2 матч между" + teams[i].name + teams[i+1].name )
+        //     }
+        //     else {
+        //         console.log('Error')
+
+        //     }
+        //     console.log(countteam)
+
+        //     if(countteam<5){
+        //         document.querySelector(".tournamenttable .left").innerHTML += `
+        //         <div>${teams[i].name}</div>
+        //         <div>${teams[i + 1].name}</div>
+        //     `
+        //     }
+        //     else{
+        //         document.querySelector(".tournamenttable .right" ).innerHTML += `
+        //         <div>${teams[i].name}</div>
+        //         <div>${teams[i + 1].name}</div>
+        //     `
+        //     }
+        
+        // }
+        // }
+        console.log(winnersArray)
+
     }
-    startMatch()
+    
     return(
         <>
         <header className = "status">
@@ -62,9 +171,13 @@ function MatchManager() {
                 )
                 )
             }</section>
+            <section className = "tournamenttable">
+                <div className="left"></div>
+                <div className="right"></div>
+            </section>
             <footer>
                 <div className = "start">
-                    <button> Start </button>
+                    <button onClick={startMatch}> Start </button>
                 </div>
             </footer>
         </>
